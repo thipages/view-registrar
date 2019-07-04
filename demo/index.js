7a3bb2a9cec94458ca382d31a1284c263d416913
thipages <thipages@gmail.com>
2019-07-04 16:26:31 +0200

    add controller.js

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,8 @@
 import {initReg, updateView} from "../esm/registrar.js";
-import {ID as ID0} from "./views/page0.js";
-import {ID as ID1} from "./views/page1.js";
 import {model, modelAccess} from "./model.js";
+import {listener} from "./controller.js";
+import {ID as ID0} from "./views/page0.js";
 const {render} = lighterhtml;
-const listener=(id,data) => {
-    if (id===ID0) updateView(ID1,"from Page 0");else updateView(ID0,"from Page 1");
-};
 const renderer=(data)=> render(document.getElementById("app"),()=>data);
 initReg(model,listener, renderer, modelAccess);
 updateView(ID0,"init");
